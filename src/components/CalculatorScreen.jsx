bebd68cc1dd20d7f892becedfa0c1a304ce42a57
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-04-11 19:16:07 +0200

    Added id to paragraph element
    This was due to the conditions on the FCC challenge
    description.

--- src/components/CalculatorScreen.jsx
+++ src/components/CalculatorScreen.jsx
@@ -1,5 +1,5 @@
 function CalculatorScreen(props) {
-	return <p className="calculator-display">{props.text}</p>;
+	return <p className="calculator-display" id="display">{props.text}</p>;
 }
 
 export default CalculatorScreen;
